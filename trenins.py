f0598c77225a03b15fc1cd9263dbe2e6bd84357f
Ralfs111 <149487291+Ralfs111@users.noreply.github.com>
2024-05-22 14:04:53 +0300

    klases_darbs

---
+++ trenins.py
@@ -0,0 +1,25 @@
+def summa():
+    skaitlis1 = 5
+    skaitlis2 = 7
+    print(skaitlis1+skaitlis2)
+    return
+
+summa("Jā", "")
+
+for skaitlis in range(1, 11, 2):      #range - funkcija, kas skaita skaitļus
+
+
+
+
+pirmais = "6"
+
+print(pirmais)
+
+vards2 = "Nē"
+
+print(vards1, vards2)
+
+
+
+def sum():
+    skaitlis 
